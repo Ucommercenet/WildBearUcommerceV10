72d577805fc4c4befb192bce27ea17cf7c7b2efb
BjoernLukas <BjoernLukasKjaer@Gmail.com>
2023-08-16 13:05:22 +0200

    Added SimpleTestController

---
+++ HeadlessProjectv1/Controllers/SimpleTestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeadlessProjectv1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleTestController : ControllerBase
+    {
+        public int AskQuestionGet()
+        {
+            return 42;
+        }
+
+        //[HttpGet("AskQuestionGet2")]
+        //public int AskQuestionGet2()
+        //{
+        //    return 85;
+        //}
+
+        //public ActionResult<string> BestAnswerGet(int guess)
+        //{
+        //    if (guess == 42)
+        //    {
+        //        return "Correct";
+        //    }
+
+        //    return this.BadRequest();
+        //}
+    }
+}
--- HeadlessProjectv1/Program.cs
+++ HeadlessProjectv1/Program.cs
@@ -1,28 +1,49 @@
-using Microsoft.AspNetCore.Authorization.Infrastructure;
 using HeadlessProjectv1;
-using Ucommerce.Web.BackOffice.DependencyInjection;
-using Ucommerce.Web.Infrastructure.DependencyInjection;
-using Ucommerce.Web.WebSite.DependencyInjection;
-using Ucommerce.Web.Core.DependencyInjection;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Ucommerce.Extensions.Payment.Abstractions.Extensions;
 using Ucommerce.Extensions.Search.Abstractions.Extensions;
 using Ucommerce.Search.Elastic.Configuration;
-using Ucommerce.Extensions.Payment.Abstractions.Extensions;
+using Ucommerce.Web.BackOffice.DependencyInjection;
+using Ucommerce.Web.Core.DependencyInjection;
+using Ucommerce.Web.Infrastructure.DependencyInjection;
+using Ucommerce.Web.WebSite.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region Default Ucommerce
 
 // Set up services
 builder.Services.AddUcommerce(builder.Configuration)
     .AddBackOffice(opt => { opt.Security.Requirements.Add(new AssertionRequirement(_ => true)); })
     .AddWebSite()
     .AddSearch()
-        .AddElasticSearch()
-        .WithSingleNodeSetup()
+    .AddElasticSearch()
+    .WithSingleNodeSetup()
     .AddPayments()
     .Build();
 
 builder.Services.AddControllers();
 
+#endregion
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Default setup from ASP.net Core API Template 
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+#region Default Ucommerce
 
 // configure and run
 app.UseTestUser();
@@ -30,5 +51,7 @@ app.UseUcommerce()
     .UseBackOfficeUi()
     .UsePayments()
     .UseEndpoints(u => { u.UseUcommerceEndpoints(); });
+
+#endregion
 
 app.Run();
