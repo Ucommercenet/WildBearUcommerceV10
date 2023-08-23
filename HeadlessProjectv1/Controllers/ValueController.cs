933c14ff548a483613fb7be5ce7abfb4f7b665f7
BjoernLukas <BjoernLukasKjaer@Gmail.com>
2023-08-23 10:49:38 +0200

    test commit

---
+++ HeadlessProjectv1/Controllers/ValueController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeadlessProjectv1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        //TODO: Just used for a test commit delete later
+    }
+}
